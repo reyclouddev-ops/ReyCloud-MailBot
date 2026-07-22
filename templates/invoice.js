fc592283a63b959add2ff43f20b0fbf2634c7401
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-22 23:15:00 +0700

    Create invoice.js

---
+++ templates/invoice.js
@@ -0,0 +1,51 @@
+module.exports=(d)=>`
+
+<h2>
+PT Legion Teknologi
+</h2>
+
+
+<h3>
+🧾 Payment Invoice
+</h3>
+
+
+<p>
+Halo ${d.nama}
+</p>
+
+
+<table>
+
+<tr>
+<td>Produk</td>
+<td>${d.produk}</td>
+</tr>
+
+
+<tr>
+<td>Harga</td>
+<td>${d.harga}</td>
+</tr>
+
+
+<tr>
+<td>Metode</td>
+<td>${d.metode}</td>
+</tr>
+
+
+<tr>
+<td>Status</td>
+<td>${d.status}</td>
+</tr>
+
+
+</table>
+
+
+<p>
+Terima kasih telah melakukan transaksi.
+</p>
+
+`;
