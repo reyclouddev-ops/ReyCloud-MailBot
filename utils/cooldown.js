ab8e2f5c89d4b80c3f8a297f2b0c617e856d2753
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-22 23:47:37 +0700

    Update cooldown.js

--- utils/cooldown.js
+++ utils/cooldown.js
@@ -1,1 +1,30 @@
+const cooldown={};
 
+
+function checkCooldown(id){
+
+
+let now=Date.now();
+
+
+if(
+cooldown[id] &&
+now-cooldown[id]<10000
+){
+
+return false;
+
+}
+
+
+cooldown[id]=now;
+
+
+return true;
+
+
+}
+
+
+
+module.exports=checkCooldown;
