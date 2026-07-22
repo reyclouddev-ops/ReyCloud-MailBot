3c42a9280b6d5e54b4bcd11fa31152149c1839cc
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-22 23:21:39 +0700

    Create callback.js

---
+++ handlers/callback.js
@@ -0,0 +1,85 @@
+const {
+    Markup
+} = require("telegraf");
+
+
+const session = require("../database/session");
+
+
+const register = require("../templates/register");
+const invoice = require("../templates/invoice");
+
+const sendMail = require("../utils/mailer");
+
+const generateMember = require("../utils/memberId");
+
+
+
+module.exports = (bot)=>{
+
+
+// BUTTON REGISTER
+
+bot.action("register", async(ctx)=>{
+
+
+let data=session[ctx.from.id];
+
+
+if(!data)
+return ctx.reply(
+"❌ Session tidak ditemukan"
+);
+
+
+
+data.template="register";
+
+data.step="nama";
+
+
+
+await ctx.reply(
+"👤 Masukkan Nama Lengkap:"
+);
+
+
+
+});
+
+
+
+
+
+// BUTTON INVOICE
+
+bot.action("invoice", async(ctx)=>{
+
+
+let data=session[ctx.from.id];
+
+
+if(!data)
+return ctx.reply(
+"❌ Session tidak ditemukan"
+);
+
+
+
+data.template="invoice";
+
+data.step="nama";
+
+
+
+await ctx.reply(
+"👤 Masukkan Nama Pelanggan:"
+);
+
+
+
+});
+
+
+
+};
