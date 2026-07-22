c1e2e0239a9a129d0e72786b90b85806a92eba24
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-22 23:14:25 +0700

    Create register.js

---
+++ templates/register.js
@@ -0,0 +1,83 @@
+module.exports=(d)=>`
+
+<h2>PT Legion Teknologi</h2>
+
+<p>
+Built in Indonesia, Trusted Worldwide.
+</p>
+
+
+<h3>
+🎉 Registration Successful
+</h3>
+
+
+<p>
+Hello ${d.nama}
+</p>
+
+
+<p>
+Data akun Anda berhasil dibuat.
+</p>
+
+
+<table>
+
+<tr>
+<td>Nama Lengkap</td>
+<td>${d.nama}</td>
+</tr>
+
+
+<tr>
+<td>Role</td>
+<td>${d.role}</td>
+</tr>
+
+
+<tr>
+<td>Email</td>
+<td>${d.email}</td>
+</tr>
+
+
+<tr>
+<td>WhatsApp</td>
+<td>${d.wa}</td>
+</tr>
+
+
+<tr>
+<td>Member ID</td>
+<td>${d.member}</td>
+</tr>
+
+
+<tr>
+<td>Status</td>
+<td>ACTIVE</td>
+</tr>
+
+
+<tr>
+<td>Tanggal</td>
+<td>${d.date}</td>
+</tr>
+
+
+</table>
+
+
+<br>
+
+<p>
+Email ini dikirim otomatis oleh sistem.
+</p>
+
+
+<b>
+PT Legion Teknologi © 2026
+</b>
+
+`;
